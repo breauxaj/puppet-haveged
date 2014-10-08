666dbabef802ad11ecbdc4ca730b66fbe2a0992b
Adam Breaux <breauxaj@gmail.com>
2014-10-08 07:59:39 -0400

    standard preamble

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,3 +1,29 @@
+# Define: haveged::service
+#
+# This define manages the haveged service
+#
+# Parameters:
+#
+#  ensure:
+#    Controls the state of the service
+#
+#  enable:
+#    Controls the service start on boot
+#
+# Actions:
+#   - Stops/starts the haveged service
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  To enable the service, use:
+#
+#    haveged::service { 'default':
+#      ensure => running,
+#      enable => true
+#    }
+#
 define haveged::service (
   $ensure,
   $enable
