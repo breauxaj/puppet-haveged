f4ea4e1f13529bb854c96dca89cfe5d139739d13
Adam Breaux <breauxaj@gmail.com>
2014-10-08 07:58:39 -0400

    standard preamble

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,31 @@
+# Class: haveged
+#
+# This class installs the haveged package
+#
+# Parameters:
+#
+#  ensure: (default latest)
+#    Determine the state of the packages
+#
+# Actions:
+#   - Installs the haveged package
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  For a standard installation, use:
+#
+#    class { 'haveged':
+#      ensure => 'latest'
+#    }
+#
+#  To remove the installation, use:
+#
+#    class { 'haveged':
+#      ensure => 'absent'
+#    }
+#
 class haveged (
   $ensure = 'latest'
 ){
